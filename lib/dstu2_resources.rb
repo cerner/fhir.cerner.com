50bcc52c5ddd87a6ef567e469c70ff19df1a049a
Sravan Shankershetty <sravi559@outlook.com>
2016-09-14 11:10:02 -0500

    Update DSTU2 documentation for Conformance with new authorize token endpoints.

--- lib/dstu2_resources.rb
+++ lib/dstu2_resources.rb
@@ -1210,7 +1210,7 @@ module Cerner
       "name": "Cerner Conformance Statement",
       "status": "draft",
       "publisher": "Cerner",
-      "date": "2015-12-03T00:00:00+00:00",
+      "date": "2016-08-15",
       "description": "Describes capabilities of this server",
       "kind": "instance",
       "fhirVersion": "1.0.2",
@@ -1226,6 +1226,14 @@ module Cerner
             "cors": true
           },
           "resource": [
+            {
+              "type": "Conformance",
+              "interaction": [
+                {
+                  "code": "read"
+                }
+              ]
+            },
             {
               "type": "AllergyIntolerance",
               "interaction": [
@@ -1257,15 +1265,42 @@ module Cerner
                 {
                   "name": "patient",
                   "type": "reference",
-                  "documentation": "The patient who has the condition. It is a required field if subject field is not given"
+                  "documentation": "The patient who has the condition. It is a required field"
+                },
+                {
+                  "name": "clinicalstatus",
+                  "type": "token",
+                  "documentation": "A list of desired clinical statuses of the condition"
                 },
                 {
                   "name": "category",
                   "type": "token",
-                  "documentation": "The category of the condition",
-                  "modifier": [
-                    "not"
-                  ]
+                  "documentation": "The category of the condition"
+                }
+              ]
+            },
+            {
+              "type": "DiagnosticReport",
+              "interaction": [
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The subject of the report if a patient. It is a required field if subject field is not given"
+                },
+                {
+                  "name": "subject",
+                  "type": "reference",
+                  "documentation": "The subject of the report. It is a required field if patient field is not given"
+                },
+                {
+                  "name": "date",
+                  "type": "date",
+                  "documentation": "The start date after which or date range in which the report occurs. If only start date is provided, must be prefixed by 'ge'. If range is provided, this parameter value should be present twice, prefixed once by 'ge' representing the earliest date and once by 'lt' representing the latest date"
                 }
               ]
             },
@@ -1290,6 +1325,44 @@ module Cerner
               ]
             },
             {
+              "type": "Encounter",
+              "interaction": [
+                {
+                  "code": "read"
+                },
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The patient present at the encounter. It is a required field if _id field is not given"
+                },
+                {
+                  "name": "_id",
+                  "type": "token",
+                  "documentation": "A single or comma separated list of Encounter ids. It is a required field if patient field is not given"
+                }
+              ]
+            },
+            {
+              "type": "Immunization",
+              "interaction": [
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The patient for the vaccination record. It is a required field"
+                }
+              ]
+            },
+            {
               "type": "MedicationOrder",
               "interaction": [
                 {
@@ -1300,22 +1373,22 @@ module Cerner
                 {
                   "name": "patient",
                   "type": "reference",
-                  "documentation": "The identity of a patient to list dispenses for. It is a required field"
+                  "documentation": "The identity of a patient to list medication orders for. It is a required field"
                 },
                 {
                   "name": "status",
                   "type": "token",
-                  "documentation": "The status of the med, may be a list separated by commas. (Active and draft statuses must be queried separately from completed, on-hold, and stopped statuses. The superseded and entered-in-error statuses are not supported). It is a required field"
+                  "documentation": "One or more medication order status values separated by commas."
                 },
                 {
-                  "name": "timing-boundsperiod-end",
+                  "name": "-timing-boundsPeriod",
                   "type": "date",
-                  "documentation": "The stop time of the order. Must be prefixed by 'le' (currently only support querying for orders completed prior to a certain time). Not accepted when querying active or draft orders"
+                  "documentation": "The date-time which should fall within the period the medication should be given to the patient. Must be prefixed by 'ge'."
                 },
                 {
                   "name": "_count",
                   "type": "number",
-                  "documentation": "The number of items to include in a page. Currently ignored if querying for active or draft statuses"
+                  "documentation": "The maximum number of results to include in a page."
                 }
               ]
             },
@@ -1324,6 +1397,9 @@ module Cerner
               "interaction": [
                 {
                   "code": "search-type"
+                },
+                {
+                  "code": "read"
                 }
               ],
               "searchParam": [
@@ -1346,6 +1422,46 @@ module Cerner
                   "name": "_count",
                   "type": "number",
                   "documentation": "The maximum number of results to include in a page."
+                },
+                {
+                  "name": "_id",
+                  "type": "token",
+                  "documentation": "One or more Medication Statement ids separated by commas. It is a required field if patient field is not given"
+                }
+              ]
+            },
+            {
+              "type": "Observation",
+              "interaction": [
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The subject that the observation is about (if patient). It is a required field if the subject field is not given"
+                },
+                {
+                  "name": "subject",
+                  "type": "reference",
+                  "documentation": "The subject that the observation is about. It is a required field if the patient field is not given"
+                },
+                {
+                  "name": "code",
+                  "type": "token",
+                  "documentation": "One or more observation types separated by commas."
+                },
+                {
+                  "name": "date",
+                  "type": "date",
+                  "documentation": "The date range into which the observation falls. This parameter value should be present twice, prefixed once by 'gt' representing the earliest date and once by 'lt' representing the latest date"
+                },
+                {
+                  "name": "_count",
+                  "type": "number",
+                  "documentation": "The maximum number of results to return in a page."
                 }
               ]
             },
@@ -1382,7 +1498,7 @@ module Cerner
                 },
                 {
                   "name": "telecom",
-                  "type": "string",
+                  "type": "token",
                   "documentation": "The value in any kind of telecom details of the patient. It is a required field if no _id, birthdate, identifier or name is given"
                 },
                 {
@@ -1407,7 +1523,7 @@ module Cerner
       "name": "Cerner Conformance Statement",
       "status": "draft",
       "publisher": "Cerner",
-      "date": "2015-12-03T00:00:00+00:00",
+      "date": "2016-08-15",
       "description": "Describes capabilities of this server",
       "kind": "instance",
       "fhirVersion": "1.0.2",
@@ -1426,11 +1542,11 @@ module Cerner
                 "extension": [
                   {
                     "url": "token",
-                    "valueUri": "https://authorization.sandboxcerner.com/realms/d075cf8b-3261-481d-97e5-ba6c48d3b41f/protocols/smart/token"
+                    "valueUri": "https://authorization.sandboxcerner.com/tenants/d075cf8b-3261-481d-97e5-ba6c48d3b41f/protocols/oauth2/profiles/smart-v1/token"
                   },
                   {
                     "url": "authorize",
-                    "valueUri": "https://authorization.sandboxcerner.com/realms/d075cf8b-3261-481d-97e5-ba6c48d3b41f/protocols/smart/authorize"
+                    "valueUri": "https://authorization.sandboxcerner.com/tenants/d075cf8b-3261-481d-97e5-ba6c48d3b41f/protocols/oauth2/profiles/smart-v1/personas/provider/authorize"
                   }
                 ]
               }
@@ -1451,12 +1567,27 @@ module Cerner
           },
           "resource": [
             {
+              "type": "Conformance",
+              "interaction": [
+                {
+                  "code": "read"
+                }
+              ]
+            },
+            {
               "type": "AllergyIntolerance",
               "interaction": [
+                {
+                  "code": "create"
+                },
+                {
+                  "code": "update"
+                },
                 {
                   "code": "search-type"
                 }
               ],
+              "updateCreate": false,
               "searchParam": [
                 {
                   "name": "patient",
@@ -1474,6 +1605,38 @@ module Cerner
               "type": "Condition",
               "interaction": [
                 {
+                  "code": "create"
+                },
+                {
+                  "code": "update"
+                },
+                {
+                  "code": "search-type"
+                }
+              ],
+              "updateCreate": false,
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The patient who has the condition. It is a required field"
+                },
+                {
+                  "name": "clinicalstatus",
+                  "type": "token",
+                  "documentation": "A list of desired clinical statuses of the condition"
+                },
+                {
+                  "name": "category",
+                  "type": "token",
+                  "documentation": "The category of the condition"
+                }
+              ]
+            },
+            {
+              "type": "DiagnosticReport",
+              "interaction": [
+                {
                   "code": "search-type"
                 }
               ],
@@ -1481,15 +1644,17 @@ module Cerner
                 {
                   "name": "patient",
                   "type": "reference",
-                  "documentation": "The patient who has the condition. It is a required field if subject field is not given"
+                  "documentation": "The subject of the report if a patient. It is a required field if subject field is not given"
                 },
                 {
-                  "name": "category",
-                  "type": "token",
-                  "documentation": "The category of the condition",
-                  "modifier": [
-                    "not"
-                  ]
+                  "name": "subject",
+                  "type": "reference",
+                  "documentation": "The subject of the report. It is a required field if patient field is not given"
+                },
+                {
+                  "name": "date",
+                  "type": "date",
+                  "documentation": "The start date after which or date range in which the report occurs. If only start date is provided, must be prefixed by 'ge'. If range is provided, this parameter value should be present twice, prefixed once by 'ge' representing the earliest date and once by 'lt' representing the latest date"
                 }
               ]
             },
@@ -1517,6 +1682,44 @@ module Cerner
               ]
             },
             {
+              "type": "Encounter",
+              "interaction": [
+                {
+                  "code": "read"
+                },
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The patient present at the encounter. It is a required field if _id field is not given"
+                },
+                {
+                  "name": "_id",
+                  "type": "token",
+                  "documentation": "A single or comma separated list of Encounter ids. It is a required field if patient field is not given"
+                }
+              ]
+            },
+            {
+              "type": "Immunization",
+              "interaction": [
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The patient for the vaccination record. It is a required field"
+                }
+              ]
+            },
+            {
               "type": "MedicationOrder",
               "interaction": [
                 {
@@ -1527,22 +1730,22 @@ module Cerner
                 {
                   "name": "patient",
                   "type": "reference",
-                  "documentation": "The identity of a patient to list dispenses for. It is a required field"
+                  "documentation": "The identity of a patient to list medication orders for. It is a required field"
                 },
                 {
                   "name": "status",
                   "type": "token",
-                  "documentation": "The status of the med, may be a list separated by commas. (Active and draft statuses must be queried separately from completed, on-hold, and stopped statuses. The superseded and entered-in-error statuses are not supported). It is a required field"
+                  "documentation": "One or more medication order status values separated by commas."
                 },
                 {
-                  "name": "timing-boundsperiod-end",
+                  "name": "-timing-boundsPeriod",
                   "type": "date",
-                  "documentation": "The stop time of the order. Must be prefixed by 'le' (currently only support querying for orders completed prior to a certain time). Not accepted when querying active or draft orders"
+                  "documentation": "The date-time which should fall within the period the medication should be given to the patient. Must be prefixed by 'ge'."
                 },
                 {
                   "name": "_count",
                   "type": "number",
-                  "documentation": "The number of items to include in a page. Currently ignored if querying for active or draft statuses"
+                  "documentation": "The maximum number of results to include in a page."
                 }
               ]
             },
@@ -1550,9 +1753,19 @@ module Cerner
               "type": "MedicationStatement",
               "interaction": [
                 {
+                  "code": "create"
+                },
+                {
+                  "code": "update"
+                },
+                {
                   "code": "search-type"
+                },
+                {
+                  "code": "read"
                 }
               ],
+              "updateCreate": false,
               "searchParam": [
                 {
                   "name": "patient",
@@ -1573,6 +1786,46 @@ module Cerner
                   "name": "_count",
                   "type": "number",
                   "documentation": "The maximum number of results to include in a page."
+                },
+                {
+                  "name": "_id",
+                  "type": "token",
+                  "documentation": "One or more Medication Statement ids separated by commas. It is a required field if patient field is not given"
+                }
+              ]
+            },
+            {
+              "type": "Observation",
+              "interaction": [
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The subject that the observation is about (if patient). It is a required field if the subject field is not given"
+                },
+                {
+                  "name": "subject",
+                  "type": "reference",
+                  "documentation": "The subject that the observation is about. It is a required field if the patient field is not given"
+                },
+                {
+                  "name": "code",
+                  "type": "token",
+                  "documentation": "One or more observation types separated by commas."
+                },
+                {
+                  "name": "date",
+                  "type": "date",
+                  "documentation": "The date range into which the observation falls. This parameter value should be present twice, prefixed once by 'gt' representing the earliest date and once by 'lt' representing the latest date"
+                },
+                {
+                  "name": "_count",
+                  "type": "number",
+                  "documentation": "The maximum number of results to return in a page."
                 }
               ]
             },
@@ -1609,7 +1862,7 @@ module Cerner
                 },
                 {
                   "name": "telecom",
-                  "type": "string",
+                  "type": "token",
                   "documentation": "The value in any kind of telecom details of the patient. It is a required field if no _id, birthdate, identifier or name is given"
                 },
                 {
