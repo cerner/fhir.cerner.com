224dc4c267898002b03843cbc0b8e3a946dae314
Bradley Strecker <bradley.strecker@cerner.com>
2016-11-14 10:02:57 -0600

    Merging and squashing pull request #83 from device

--- lib/resources/example_json/dstu2_examples_conformance.rb
+++ lib/resources/example_json/dstu2_examples_conformance.rb
@@ -81,6 +81,29 @@ module Cerner
               ]
             },
             {
+              "type": "Device",
+              "interaction": [
+                {
+                  "code": "read"
+                },
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The patient who has the device affixed. It is a required field if _id field is not given"
+                },
+                {
+                  "name": "_id",
+                  "type": "token",
+                  "documentation": "A single or comma separated list of Device ids. It is a required field if patient field is not given"
+                }
+              ]
+            },
+            {
               "type": "DiagnosticReport",
               "interaction": [
                 {
@@ -431,6 +454,29 @@ module Cerner
                   "name": "category",
                   "type": "token",
                   "documentation": "The category of the condition"
+                }
+              ]
+            },
+            {
+              "type": "Device",
+              "interaction": [
+                {
+                  "code": "read"
+                },
+                {
+                  "code": "search-type"
+                }
+              ],
+              "searchParam": [
+                {
+                  "name": "patient",
+                  "type": "reference",
+                  "documentation": "The patient who has the device affixed. It is a required field if _id field is not given"
+                },
+                {
+                  "name": "_id",
+                  "type": "token",
+                  "documentation": "A single or comma separated list of Device ids. It is a required field if patient field is not given"
                 }
               ]
             },
