2565394922101542ebcad3fb6cf92168a0ab82ec
Madhur Rajendran <madhur.rajendran@cerner.com>
2017-02-02 16:18:30 -0600

    Adding template for new resources

---
+++ lib/resources/example_json/dstu2_examples_resource_example.rb
@@ -0,0 +1,14 @@
+module Cerner
+  module Resources
+
+    DSTU2_RESOURCE_EXAMPLE_ENTRY ||= {
+      # Add your resource here.
+    }
+
+
+    DSTU2_RESOURCE_EXAMPLE_BUNDLE ||= {
+      # Add a bundle of your resource here.
+    }
+
+  end
+end
