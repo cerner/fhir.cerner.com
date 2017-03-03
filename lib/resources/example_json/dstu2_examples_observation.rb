404160693cea0d4fad68488800f2b41337ea27ba
Max Philips <mphilips890@gmail.com>
2017-03-03 09:14:01 -0600

    Update Observation documentation: * Remove component-code parameter * General tweaks for clarity

--- lib/resources/example_json/dstu2_examples_observation.rb
+++ lib/resources/example_json/dstu2_examples_observation.rb
@@ -9,7 +9,7 @@ module Cerner
         "link": [
           {
             "relation": "self",
-            "url": "https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Observation?subject%3APatient=3998008&_count=50"
+            "url": "https://fhir-open.sandboxcerner.com/dstu2/0b8a0111-e8e6-4c26-a91c-5069cbc6b1ca/Observation?patient=3998008&_count=50"
           }
         ],
         "entry": [
