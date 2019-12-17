b059a8899514f91baa449669f35f9889fb89d5bf
Thushar <thushar.sabu@cerner.com>
2019-12-17 16:41:11 -0600

    Addressed Pr comments

--- lib/resources/example_json/r4_example_binary.rb
+++ lib/resources/example_json/r4_example_binary.rb
@@ -7,7 +7,7 @@ module Cerner
           "lastUpdated": "2019-12-16T18:50:47.287+00:00"
       },
       "contentType": "application/pdf",
-      "data": "JVBERi0xLjQKJeLjz9MKMSAwIG9iago8PC9FbmNvZGlu... <!-- snipped for brevity -->"
+      "data": "JVBERi0xLjQKJeLjz9MKMSAwIG9iago8PC9FbmNvZGlu... < snipped for brevity >"
     }
   end
 end
