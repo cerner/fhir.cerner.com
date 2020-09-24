3466a9a87036c630d1aaf631f039f0db21b111c8
Santanu Banerjee <56925992+SantanuBanerjee@users.noreply.github.com>
2020-09-25 01:45:30 +0530

    Added Encounter patch for payment-collection-status extension (#465)

--- lib/resources/example_json/r4_examples_encounter.rb
+++ lib/resources/example_json/r4_examples_encounter.rb
@@ -1132,6 +1132,26 @@ module Cerner
             ]
           }
         ]
+      },
+      {
+        'op': 'replace',
+        'path': '/extension',
+        'value': [
+          {
+            'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/payment-collection-status',
+            'valueCodeableConcept': {
+              'coding': [
+                {
+                  'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4003489',
+                  'code': '269411463',
+                  'display': '1st Attempt - Left Message',
+                  'userSelected': true
+                }
+              ],
+              'text': '1st Attempt - Left Message'
+            }
+          }
+        ]
       }
     ].freeze
   end
