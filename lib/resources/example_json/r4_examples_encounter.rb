1e31aa5a5db805a8d1445bc8aa4cbb3b98c173ba
Sydne Anschutz <sydne.anschutz@cerner.com>
2020-10-01 10:09:36 -0500

    lint

--- lib/resources/example_json/r4_examples_encounter.rb
+++ lib/resources/example_json/r4_examples_encounter.rb
@@ -1146,15 +1146,15 @@ module Cerner
         'op': 'replace',
         'path': '/extension',
         'value': [
-           {
-             'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/estimated-financial-responsibility-amount',
-             'valueMoney': {
-               'value': 10.0,
-               'currency': 'USD'
-             }
-           }
-         ]
-       }
+          {
+            'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/estimated-financial-responsibility-amount',
+            'valueMoney': {
+              'value': 10.0,
+              'currency': 'USD'
+            }
+          }
+        ]
+      }
     ].freeze
   end
 end
