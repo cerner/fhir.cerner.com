29bd229d9932005285790d02f4c6a5b4c3b5bfcc
NimishaMN <nimisha.mn@gmail.com>
2020-10-05 20:48:06 +0530

    minor fix

--- lib/resources/example_json/r4_examples_care_team.rb
+++ lib/resources/example_json/r4_examples_care_team.rb
@@ -4,54 +4,54 @@ module Cerner
   module Resources
 
     R4_CARE_TEAM_ENTRY ||= {
-      "resourceType": "CareTeam",
-      "id": "CT-119078440-34768489-17490349-4048128-0-22554767",
-      "meta": {
-          "versionId": 1584980008,
-          "lastUpdated": "2020-03-23T16:13:28Z"
+      'resourceType': 'CareTeam',
+      'id': 'CT-119078440-34768489-17490349-4048128-0-22554767',
+      'meta': {
+          'versionId': 1584980008,
+          'lastUpdated': '2020-03-23T16:13:28Z'
       },
-      "text": {
-          "status": "generated",
-          "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Care Team</b></p><p><b>Patient</b>: FHIR, FIRE3</p><p><b>Care Team Name</b>: Psychology;Blue Team</p><p><b>Status</b>: Active</p><p><b>Category</b>: Encounter-focused care team</p><p><b>Encounter</b>: 34768489</p><p><b>Participant</b>:</p><ul><li>Name:Dickey, Bo; Role:Psychologist (occupation)</li></ul></div>"
+      'text': {
+          'status': 'generated',
+          'div': '<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Care Team</b></p><p><b>Patient</b>: FHIR, FIRE3</p><p><b>Care Team Name</b>: Psychology;Blue Team</p><p><b>Status</b>: Active</p><p><b>Category</b>: Encounter-focused care team</p><p><b>Encounter</b>: 34768489</p><p><b>Participant</b>:</p><ul><li>Name:Dickey, Bo; Role:Psychologist (occupation)</li></ul></div>'
       },
-      "status": "active",
-      "category": [
+      'status': 'active',
+      'category': [
           {
-              "coding": [
+              'coding': [
                   {
-                      "system": "http://loinc.org",
-                      "code": "LA27976-2",
-                      "display": "Encounter-focused care team"
+                      'system': 'http://loinc.org',
+                      'code': 'LA27976-2',
+                      'display': 'Encounter-focused care team'
                   }
               ],
-              "text": "Encounter-focused care team"
+              'text': 'Encounter-focused care team'
           }
       ],
-      "name": "Psychology;Blue Team",
-      "subject": {
-          "reference": "Patient/119078440",
-          "display": "FHIR, FIRE3"
+      'name': 'Psychology;Blue Team',
+      'subject': {
+          'reference': 'Patient/119078440',
+          'display': 'FHIR, FIRE3'
       },
-      "encounter": {
-          "reference": "Encounter/34768489"
+      'encounter': {
+          'reference': 'Encounter/34768489'
       },
-      "participant": [
+      'participant': [
           {
-              "role": [
+              'role': [
                   {
-                      "coding": [
+                      'coding': [
                           {
-                              "system": "http://snomed.info/sct",
-                              "code": "59944000",
-                              "display": "Psychologist (occupation)"
+                              'system': 'http://snomed.info/sct',
+                              'code': '59944000',
+                              'display': 'Psychologist (occupation)'
                           }
                       ],
-                      "text": "Psychologist (occupation)"
+                      'text': 'Psychologist (occupation)'
                   }
               ],
-              "member": {
-                  "reference": "Practitioner/17490349",
-                  "display": "Dickey, Bo"
+              'member': {
+                  'reference': 'Practitioner/17490349',
+                  'display': 'Dickey, Bo'
               }
           }
       ]
