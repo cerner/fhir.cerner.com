1c10d341767366f091f35d9350c93248e4a16e29
root <root@W1861286.northamerica.cerner.net>
2020-12-17 12:58:39 +0530

    addressing comments

--- lib/resources/example_json/r4_examples_medication_request.rb
+++ lib/resources/example_json/r4_examples_medication_request.rb
@@ -13,13 +13,13 @@ module Cerner
         },
         'text': {
           'status': 'generated',
-          'div': '&lt;div xmlns=&#39;http://www.w3.org/1999/xhtml&#39;&gt;&lt;p&gt;&lt;b&gt;Medication Request&lt;'\
-                 '/b&gt;&lt;/p&gt;&lt;p&gt;&lt;b&gt;Status&lt;/b&gt;: Active&lt;/p&gt;&lt;p&gt;&lt;b&gt;Intent&lt;'\
-                 '/b&gt;: Order&lt;/p&gt;&lt;p&gt;&lt;b&gt;Medication&lt;/b&gt;'\
-                 ': hepatitis B adult vaccine (hepatitis B adult vaccine dialysis 40 mcg/mL intramuscular suspension)'\
-                 '&lt;/p&gt;&lt;p&gt;&lt;b&gt;Dosage Instructions&lt;/b&gt;: 40 mcg = 1 mL, IM, Once&lt;/p&gt;&lt;p'\
-                 '&gt;&lt;b&gt;Patient&lt;/b&gt;: SMART, NANCY&lt;/p&gt;&lt;p&gt;&lt;b&gt;Authored On&lt;'\
-                 '/b&gt;: Jul  6, 2020  8:37 P.M. UTC&lt;/p&gt;&lt;/div&gt;'
+          'div': '<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Medication Request</b></p>\
+          <p><b>Status</b>: Active</p>\
+          <p><b>Intent</b>: Order</p>\
+          <p><b>Medication</b>: lisinopril-hydroCHLOROthiazide
+          (lisinopril-hydroCHLOROthiazide 10 mg-12.5 mg oral tablet)</p>\
+          <p><b>Dosage Instructions</b>: 1 tab, Oral, Daily</p>\
+          <p><b>Patient</b>: PETERS, TIM A</p><p><b>Authored On</b>: Nov 21, 2020  8:59 P.M. UTC</p></div>'
         },
         'status': 'active',
         'intent': 'order',
@@ -27,7 +27,7 @@ module Cerner
           {
             'coding': [
               {
-                "system": 'http://terminology.hl7.org/CodeSystem/medicationrequest-category',
+                'system': 'http://terminology.hl7.org/CodeSystem/medicationrequest-category',
                 'code': 'inpatient',
                 'display': 'Inpatient',
                 'userSelected': false
@@ -39,13 +39,13 @@ module Cerner
         'medicationCodeableConcept': {
           'coding': [
             {
-              "system": 'http://www.nlm.nih.gov/research/umls/rxnorm',
+              'system': 'http://www.nlm.nih.gov/research/umls/rxnorm',
               'code': '830261',
               'display': 'Hepatitis B Surface Antigen Vaccine 0.04 MG/ML Injectable Suspension',
               'userSelected': false
             },
             {
-              "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/synonym',
+              'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/synonym',
               'code': '19953289',
               'display': 'hepatitis B adult vaccine dialysis 40 mcg/mL intramuscular suspension',
               'userSelected': true
@@ -68,13 +68,13 @@ module Cerner
         'courseOfTherapyType': {
           'coding': [
             {
-              "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4009',
+              'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4009',
               'code': '2337',
               'display': 'Physician Stop',
               'userSelected': true
             },
             {
-              "system": 'http://terminology.hl7.org/CodeSystem/medicationrequest-course-of-therapy',
+              'system': 'http://terminology.hl7.org/CodeSystem/medicationrequest-course-of-therapy',
               'code': 'acute',
               'display': 'Short course (acute) therapy',
               'userSelected': false
@@ -84,7 +84,7 @@ module Cerner
         },
         'dosageInstruction': [
           {
-            "extension": [
+            'extension': [
               {
                 'valueString': '40 mcg = 1 mL, IM, Once, First Dose: 12/07/16 16:00:00 CST',
                 'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/clinical-instruction'
@@ -102,7 +102,7 @@ module Cerner
               'code': {
                 'coding': [
                   {
-                    "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4003',
+                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4003',
                     'code': '696531',
                     'display': 'Once',
                     'userSelected': true
@@ -114,13 +114,13 @@ module Cerner
             'route': {
               'coding': [
                 {
-                  "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4001',
+                  'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4001',
                   'code': '318167',
                   'display': 'IM',
                   'userSelected': true
                 },
                 {
-                  "system": 'http://snomed.info/sct',
+                  'system': 'http://snomed.info/sct',
                   'code': '78421000',
                   'display': 'Intramuscular route (qualifier value)',
                   'userSelected': false
@@ -133,7 +133,7 @@ module Cerner
                 'doseQuantity': {
                   'value': 1.0,
                   'unit': 'mL',
-                  "system": 'http://unitsofmeasure.org',
+                  'system': 'http://unitsofmeasure.org',
                   'code': 'mL'
                 }
               }
@@ -149,166 +149,166 @@ module Cerner
     }.freeze
 
     R4_MEDICATION_REQUEST_PATIENT_ENTRY ||= {
-      "resourceType": 'MedicationRequest',
-      "id": '314010287',
-      "meta": {
-        "versionId": '0',
-        "lastUpdated": '2020-07-14T14:45:37.000Z'
+      'resourceType': 'MedicationRequest',
+      'id': '314010287',
+      'meta': {
+        'versionId': '0',
+        'lastUpdated': '2020-07-14T14:45:37.000Z'
       },
-      "text": {
-        "status": 'generated',
-        "div": '<div xmlns="http://www.w3.org/1999/xhtml"><p><b>Medication Request</b></p>\
+      'text': {
+        'status': 'generated',
+        'div': '<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Medication Request</b></p>\
         <p><b>Status</b>: Active</p><p><b>Intent</b>: Order</p>\
         <p><b>Medication</b>: acetaminophen (acetaminophen 325 mg oral capsule)</p>\
         <p><b>Dosage Instructions</b>: 650 mg = 2 cap, Oral, every 4 hr,
          PRN: as needed for pain, 90 cap, 0 Refill(s)</p>\
         <p><b>Patient</b>: SMART, WILMA</p><p><b>Authored On</b>: Jul 14, 2020  2:45 P.M. UTC</p></div>'
       },
-      "status": 'active',
-      "intent": 'order',
-      "category": [
+      'status': 'active',
+      'intent': 'order',
+      'category': [
         {
-          "coding": [
+          'coding': [
             {
-              "system": 'http://terminology.hl7.org/CodeSystem/medicationrequest-category',
-              "code": 'community',
-              "display": 'Community',
-              "userSelected": false
+              'system': 'http://terminology.hl7.org/CodeSystem/medicationrequest-category',
+              'code': 'community',
+              'display': 'Community',
+              'userSelected': false
             }
           ]
         },
         {
-          "coding": [
+          'coding': [
             {
-              "system": 'http://terminology.hl7.org/CodeSystem/medicationrequest-category',
-              "code": 'discharge',
-              "display": 'Discharge',
-              "userSelected": false
+              'system': 'http://terminology.hl7.org/CodeSystem/medicationrequest-category',
+              'code': 'discharge',
+              'display': 'Discharge',
+              'userSelected': false
             }
           ]
         }
       ],
-      "reportedBoolean": false,
-      "medicationCodeableConcept": {
-        "coding": [
+      'reportedBoolean': false,
+      'medicationCodeableConcept': {
+        'coding': [
           {
-            "system": 'http://www.nlm.nih.gov/research/umls/rxnorm',
-            "code": '198436',
-            "display": 'Acetaminophen 325 MG Oral Capsule'
+            'system': 'http://www.nlm.nih.gov/research/umls/rxnorm',
+            'code': '198436',
+            'display': 'Acetaminophen 325 MG Oral Capsule'
           },
           {
-            "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/synonym',
-            "code": '276230589',
-            "display": 'acetaminophen 325 mg oral capsule'
+            'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/synonym',
+            'code': '276230589',
+            'display': 'acetaminophen 325 mg oral capsule'
           }
         ],
-        "text": 'acetaminophen (acetaminophen 325 mg oral capsule)'
+        'text': 'acetaminophen (acetaminophen 325 mg oral capsule)'
       },
-      "subject": {
-        "reference": 'Patient/12724065',
-        "display": 'SMART, WILMA'
+      'subject': {
+        'reference': 'Patient/12724065',
+        'display': 'SMART, WILMA'
       },
-      "encounter": {
-        "reference": 'Encounter/97953483'
+      'encounter': {
+        'reference': 'Encounter/97953483'
       },
-      "authoredOn": '2020-07-14T09:45:15.000-05:00',
-      "requester": {
-        "reference": 'Practitioner/12732044',
-        "display": 'PWMedActionPlan, Physician - Primary Care'
+      'authoredOn': '2020-07-14T09:45:15.000-05:00',
+      'requester': {
+        'reference': 'Practitioner/12732044',
+        'display': 'PWMedActionPlan, Physician - Primary Care'
       },
-      "courseOfTherapyType": {
-        "coding": [
+      'courseOfTherapyType': {
+        'coding': [
           {
-            "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4009',
-            "code": '2338',
-            "display": 'Soft Stop'
+            'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4009',
+            'code': '2338',
+            'display': 'Soft Stop'
           },
           {
-            "system": 'http://terminology.hl7.org/CodeSystem/medicationrequest-course-of-therapy',
-            "code": 'continuous',
-            "display": 'Continuous long term therapy'
+            'system': 'http://terminology.hl7.org/CodeSystem/medicationrequest-course-of-therapy',
+            'code': 'continuous',
+            'display': 'Continuous long term therapy'
           }
         ],
-        "text": 'Maintenance'
+        'text': 'Maintenance'
       },
-      "dosageInstruction": [
+      'dosageInstruction': [
         {
-          "text": '650 mg = 2 cap, Oral, every 4 hr, PRN: as needed for pain, 90 cap, 0 Refill(s)',
-          "patientInstruction": '2 Capsules Oral (given by mouth) every 4 hours as needed as needed for pain.
+          'text': '650 mg = 2 cap, Oral, every 4 hr, PRN: as needed for pain, 90 cap, 0 Refill(s)',
+          'patientInstruction': '2 Capsules Oral (given by mouth) every 4 hours as needed as needed for pain.
            Refills: 0.',
-          "timing": {
-            "repeat": {
-              "boundsPeriod": {
-                "start": '2020-07-14T09:45:00.000-05:00'
+          'timing': {
+            'repeat': {
+              'boundsPeriod': {
+                'start': '2020-07-14T09:45:00.000-05:00'
               }
             },
-            "code": {
-              "coding": [
+            'code': {
+              'coding': [
                 {
-                  "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4003',
-                  "code": '696547',
-                  "display": 'every 4 hr'
+                  'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4003',
+                  'code': '696547',
+                  'display': 'every 4 hr'
                 },
                 {
-                  "system": 'http://terminology.hl7.org/CodeSystem/v3-GTSAbbreviation',
-                  "code": 'Q4H',
-                  "display": 'Q4H'
+                  'system': 'http://terminology.hl7.org/CodeSystem/v3-GTSAbbreviation',
+                  'code': 'Q4H',
+                  'display': 'Q4H'
                 },
                 {
-                  "system": 'http://snomed.info/sct',
-                  "code": '225756002',
-                  "display": 'Every four hours (qualifier value)'
+                  'system': 'http://snomed.info/sct',
+                  'code': '225756002',
+                  'display': 'Every four hours (qualifier value)'
                 }
               ],
-              "text": 'every 4 hr'
+              'text': 'every 4 hr'
             }
           },
-          "asNeededCodeableConcept": {
-            "text": 'as needed for pain'
+          'asNeededCodeableConcept': {
+            'text': 'as needed for pain'
           },
-          "route": {
-            "coding": [
+          'route': {
+            'coding': [
               {
-                "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4001',
-                "code": '318185',
-                "display": 'Oral',
-                "userSelected": true
+                'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4001',
+                'code': '318185',
+                'display': 'Oral',
+                'userSelected': true
               },
               {
-                "system": 'http://snomed.info/sct',
-                "code": '26643006',
-                "display": 'Oral route (qualifier value)',
-                "userSelected": false
+                'system': 'http://snomed.info/sct',
+                'code': '26643006',
+                'display': 'Oral route (qualifier value)',
+                'userSelected': false
               }
             ],
-            "text": 'Oral'
+            'text': 'Oral'
           },
-          "doseAndRate": [
+          'doseAndRate': [
             {
-              "doseQuantity": {
-                "value": 2,
-                "unit": 'cap',
-                "system": 'http://unitsofmeasure.org',
-                "code": '{Capsule}'
+              'doseQuantity': {
+                'value': 2,
+                'unit': 'cap',
+                'system': 'http://unitsofmeasure.org',
+                'code': '{Capsule}'
               }
             }
           ]
         }
       ],
-      "dispenseRequest": {
-        "validityPeriod": {
-          "start": '2020-07-14T09:45:15.000-05:00'
+      'dispenseRequest': {
+        'validityPeriod': {
+          'start': '2020-07-14T09:45:15.000-05:00'
         },
-        "numberOfRepeatsAllowed": 0,
-        "quantity": {
-          "value": 90,
-          "unit": 'cap',
-          "system": 'http://unitsofmeasure.org',
-          "code": '{Capsule}'
+        'numberOfRepeatsAllowed': 0,
+        'quantity': {
+          'value': 90,
+          'unit': 'cap',
+          'system': 'http://unitsofmeasure.org',
+          'code': '{Capsule}'
         }
       },
-      "substitution": {
-        "allowedBoolean": true
+      'substitution': {
+        'allowedBoolean': true
       }
     }.freeze
 
@@ -321,7 +321,7 @@ module Cerner
       },
       'text': {
         'status': 'generated',
-        'div': '<div xmlns="http://www.w3.org/1999/xhtml"><p><b>Medication Request</b></p>\
+        'div': '<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Medication Request</b></p>\
         <p><b>Status</b>: Entered in Error</p><p><b>Intent</b>: Order</p>/
         <p><b>Medication</b>: Error</p><p><b>Patient</b>: MARSTON, JACK</p></div>'
       },
@@ -330,7 +330,7 @@ module Cerner
       'medicationCodeableConcept': {
         'coding': [
           {
-            "system": 'http://terminology.hl7.org/CodeSystem/data-absent-reason',
+            'system': 'http://terminology.hl7.org/CodeSystem/data-absent-reason',
             'code': 'error',
             'display': 'Error'
           }
@@ -390,7 +390,7 @@ module Cerner
       'medicationCodeableConcept': {
         'coding': [
           {
-            "system": 'http://www.nlm.nih.gov/research/umls/rxnorm',
+            'system': 'http://www.nlm.nih.gov/research/umls/rxnorm',
             'code': '352362'
           }
         ],
@@ -430,7 +430,7 @@ module Cerner
             'code': {
               'coding': [
                 {
-                  "system": 'http://terminology.hl7.org/CodeSystem/v3-GTSAbbreviation',
+                  'system': 'http://terminology.hl7.org/CodeSystem/v3-GTSAbbreviation',
                   'code': 'BID'
                 }
               ]
@@ -440,7 +440,7 @@ module Cerner
           'site': {
             'coding': [
               {
-                "system": 'http://snomed.info/sct',
+                'system': 'http://snomed.info/sct',
                 'code': '368208006'
               }
             ]
@@ -448,7 +448,7 @@ module Cerner
           'route': {
             'coding': [
               {
-                "system": 'http://snomed.info/sct',
+                'system': 'http://snomed.info/sct',
                 'code': '41974700'
               }
             ]
@@ -458,7 +458,7 @@ module Cerner
               'doseQuantity': {
                 'value': 23.0,
                 'unit': 'tabs',
-                "system": 'http://unitsofmeasure.org',
+                'system': 'http://unitsofmeasure.org',
                 'code': 'tbl'
               }
             }
@@ -469,7 +469,7 @@ module Cerner
         'numberOfRepeatsAllowed': 5,
         'quantity': {
           'value': 10.0,
-          "system": 'http://unitsofmeasure.org',
+          'system': 'http://unitsofmeasure.org',
           'code': 'tbl'
         }
       },
