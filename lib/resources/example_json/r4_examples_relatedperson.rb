b5231d66e5756faf6981fa501b35e18fad66554a
Lauren Dumler <lauren.dumler@cerner.com>
2021-01-19 15:50:01 -0600

    Updated RelatedPerson name.id

--- lib/resources/example_json/r4_examples_relatedperson.rb
+++ lib/resources/example_json/r4_examples_relatedperson.rb
@@ -88,7 +88,7 @@ module Cerner
       ],
       'name': [
         {
-          'id': 'CI-12724068-12724066-0',
+          'id': 'CI-12724068-0',
           'use': 'official',
           'text': 'SMART, HAILEY',
           'family': 'SMART',
@@ -385,7 +385,7 @@ module Cerner
             ],
             'name': [
               {
-                'id': 'CI-12724069-12724066-0',
+                'id': 'CI-12724066-0',
                 'use': 'official',
                 'text': 'SMART, TIMMY',
                 'family': 'SMART',
