bf0e81f3fedb01788b24e95671ea2f1d5a0e3033
SM025316 <sangeetha.m@cerner.com>
2021-02-26 16:04:06 +0530

    lint issues fixed

--- lib/resources/example_json/r4_examples_service_request.rb
+++ lib/resources/example_json/r4_examples_service_request.rb
@@ -185,40 +185,41 @@ module Cerner
             },
             'text': {
               'status': 'generated',
-              'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p><p><b>Patient</b>: FRIAS,</p>'\
-              '<p><b>Code</b>: Pain Assessment Adult</p><p><b>Occurrence</b>: every 4 hr</p><p><b>Priority</b>: Routine</p><'\
-              'p><b>Status</b>: Entered in Error</p><p><b>Intent</b>: Order</p><p><b>Requester</b>: Martin, MD, Mary Cerner</p>'\
+              'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p>'\
+              '<p><b>Patient</b>: FRIAS,</p><p><b>Code</b>: Pain Assessment Adult</p>'\
+              '<p><b>Occurrence</b>: every 4 hr</p><p><b>Priority</b>: Routine</p><p><b>Status</b>: Entered in Error'\
+              '</p><p><b>Intent</b>: Order</p><p><b>Requester</b>: Martin, MD, Mary Cerner</p>'\
               '<p><b>Order Detail</b>: 12/08/16 9:41:00 CST, every 4 hr</p></div>'
             },
             'status': 'entered-in-error',
             'intent': 'order',
             'category': [
-                {
-                  'coding': [
-                      {
-                          'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/6000',
-                          'code': '2515',
-                          'display': 'Patient Care',
-                          'userSelected': true
-                      }
-                  ],
-                  'text': 'Patient Care'
-                }
+              {
+                'coding': [
+                  {
+                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/6000',
+                    'code': '2515',
+                    'display': 'Patient Care',
+                    'userSelected': true
+                  }
+                ],
+                'text': 'Patient Care'
+              }
             ],
             'priority': 'routine',
             'code': {
               'coding': [
-                  {
-                      'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/200',
-                      'code': '2780687',
-                      'userSelected': true
-                  }
+                {
+                  'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/200',
+                  'code': '2780687',
+                  'userSelected': true
+                }
               ],
               'text': 'Pain Assessment Adult'
             },
             'orderDetail': [
               {
-                  'text': '12/08/16 9:41:00 CST, every 4 hr'
+                'text': '12/08/16 9:41:00 CST, every 4 hr'
               }
             ],
             'subject': {
@@ -231,24 +232,24 @@ module Cerner
             'occurrenceTiming': {
               'repeat': {
                 'boundsPeriod': {
-                    'start': '2016-12-08T09:41:00.000-06:00',
-                    'end': '2018-02-02T14:01:59.000-06:00'
+                  'start': '2016-12-08T09:41:00.000-06:00',
+                  'end': '2018-02-02T14:01:59.000-06:00'
                 }
               },
               'code': {
                 'coding': [
-                    {
-                      'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4003',
-                      'code': '696547',
-                      'display': 'every 4 hr',
-                      'userSelected': true
-                    },
-                    {
-                      'system': 'http://terminology.hl7.org/CodeSystem/v3-GTSAbbreviation',
-                      'code': 'Q4H',
-                      'display': 'Q4H',
-                      'userSelected': false
-                    }
+                  {
+                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4003',
+                    'code': '696547',
+                    'display': 'every 4 hr',
+                    'userSelected': true
+                  },
+                  {
+                    'system': 'http://terminology.hl7.org/CodeSystem/v3-GTSAbbreviation',
+                    'code': 'Q4H',
+                    'display': 'Q4H',
+                    'userSelected': false
+                  }
                 ],
                 'text': 'every 4 hr'
               }
@@ -268,68 +269,69 @@ module Cerner
         {
           'fullUrl': 'https://fhir-open.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/ServiceRequest/294664221',
           'resource': {
-              'resourceType': 'ServiceRequest',
-              'id': '294664221',
-              'meta': {
-                'versionId': '1',
-                'lastUpdated': '2016-12-29T02:00:21.000Z'
-              },
-              'text': {
-                'status': 'generated',
-                'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p><p><b>Patient</b>: FRIAS,</p>'\
-                '<p><b>Code</b>: Consult to Case Management</p><p><b>Occurrence</b>: Dec  8, 2016  3:41 P.M. UTC</p>'\
-                '<p><b>Priority</b>: Routine</p><p><b>Status</b>: Revoked</p><p><b>Intent</b>: Order</p>'\
-                '<p><b>Requester</b>: Martin, MD, Mary Cerner</p><p><b>Order Detail</b>: 12/08/16 9:41:00 CST, Routine</p></div>'
-              },
-              'status': 'revoked',
-              'intent': 'order',
-              'category': [
-                {
-                  'coding': [
-                    {
-                      'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/6000',
-                      'code': '636063',
-                      'display': 'Consults',
-                      'userSelected': true
-                    }
-                  ],
-                  'text': 'Consults'
-                }
-              ],
-              'priority': 'routine',
-              'code': {
+            'resourceType': 'ServiceRequest',
+            'id': '294664221',
+            'meta': {
+              'versionId': '1',
+              'lastUpdated': '2016-12-29T02:00:21.000Z'
+            },
+            'text': {
+              'status': 'generated',
+              'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p>'\
+              '<p><b>Patient</b>: FRIAS,</p><p><b>Code</b>: Consult to Case Management</p>'\
+              '<p><b>Occurrence</b>: Dec  8, 2016  3:41 P.M. UTC</p><p><b>Priority</b>: Routine</p>'\
+              '<p><b>Status</b>: Revoked</p><p><b>Intent</b>: Order</p><p><b>Requester'\
+              '</b>: Martin, MD, Mary Cerner</p><p><b>Order Detail</b>: 12/08/16 9:41:00 CST, Routine</p></div>'
+            },
+            'status': 'revoked',
+            'intent': 'order',
+            'category': [
+              {
                 'coding': [
                   {
-                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/200',
-                    'code': '2551292823',
+                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/6000',
+                    'code': '636063',
+                    'display': 'Consults',
                     'userSelected': true
                   }
                 ],
-                'text': 'Consult to Case Management'
-              },
-              'orderDetail': [
+                'text': 'Consults'
+              }
+            ],
+            'priority': 'routine',
+            'code': {
+              'coding': [
                 {
-                  'text': '12/08/16 9:41:00 CST, Routine'
+                  'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/200',
+                  'code': '2551292823',
+                  'userSelected': true
                 }
               ],
-              'subject': {
-                'reference': 'Patient/12457977',
-                'display': 'FRIAS,'
-              },
-              'encounter': {
-                'reference': 'Encounter/97697398'
-              },
-              'occurrenceDateTime': '2016-12-08T09:41:00.000-06:00',
-              'authoredOn': '2016-12-08T09:41:54.000-06:00',
-              'requester': {
-                'reference': 'Practitioner/11724002',
-                'display': 'Martin, MD, Mary Cerner'
-              },
-              'note': [
-                {
-                  'text': 'Appropriate discharge referrals and schedule follow-up appointment before discharge'
-                }
-              ]
+              'text': 'Consult to Case Management'
+            },
+            'orderDetail': [
+              {
+                'text': '12/08/16 9:41:00 CST, Routine'
+              }
+            ],
+            'subject': {
+              'reference': 'Patient/12457977',
+              'display': 'FRIAS,'
+            },
+            'encounter': {
+              'reference': 'Encounter/97697398'
+            },
+            'occurrenceDateTime': '2016-12-08T09:41:00.000-06:00',
+            'authoredOn': '2016-12-08T09:41:54.000-06:00',
+            'requester': {
+              'reference': 'Practitioner/11724002',
+              'display': 'Martin, MD, Mary Cerner'
+            },
+            'note': [
+              {
+                'text': 'Appropriate discharge referrals and schedule follow-up appointment before discharge'
+              }
+            ]
           }
         }
       ]
@@ -340,10 +342,10 @@ module Cerner
       'id': '209b1587-251d-4ac8-9d6d-35660e9476fb',
       'type': 'searchset',
       'link': [
-          {
-              'relation': 'self',
-              'url': 'https://fhir-ehr.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/ServiceRequest?patient=12457977'
-          }
+        {
+          'relation': 'self',
+          'url': 'https://fhir-ehr.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/ServiceRequest?patient=12457977'
+        }
       ],
       'entry': [
         {
@@ -357,8 +359,8 @@ module Cerner
             },
             'text': {
               'status': 'generated',
-              'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p><p><b>Patient</b>: FRIAS,</p>'\
-              '<p><b>Status</b>: Entered in Error</p><p><b>Intent</b>: Order</p></div>'
+              'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p>'\
+              '<p><b>Patient</b>: FRIAS,</p><p><b>Status</b>: Entered in Error</p><p><b>Intent</b>: Order</p></div>'
             },
             'status': 'entered-in-error',
             'intent': 'order',
@@ -371,58 +373,59 @@ module Cerner
         {
           'fullUrl': 'https://fhir-ehr.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/ServiceRequest/294664221',
           'resource': {
-              'resourceType': 'ServiceRequest',
-              'id': '294664221',
-              'meta': {
-                'versionId': '1',
-                'lastUpdated': '2016-12-29T02:00:21.000Z'
-              },
-              'text': {
-                'status': 'generated',
-                'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p><p><b>Patient</b>: FRIAS,</p>'\
-                '<p><b>Code</b>: Consult to Case Management</p><p><b>Occurrence</b>: Dec  8, 2016  3:41 P.M. UTC</p>'\
-                '<p><b>Priority</b>: Routine</p><p><b>Status</b>: Revoked</p><p><b>Intent</b>: Order</p>'\
-                '<p><b>Requester</b>: Martin, MD, Mary Cerner</p></div>'
-              },
-              'status': 'revoked',
-              'intent': 'order',
-              'category': [
-                {
-                  'coding': [
-                    {
-                      'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/6000',
-                      'code': '636063',
-                      'display': 'Consults',
-                      'userSelected': true
-                    }
-                  ],
-                  'text': 'Consults'
-                }
-              ],
-              'priority': 'routine',
-              'code': {
+            'resourceType': 'ServiceRequest',
+            'id': '294664221',
+            'meta': {
+              'versionId': '1',
+              'lastUpdated': '2016-12-29T02:00:21.000Z'
+            },
+            'text': {
+              'status': 'generated',
+              'div': '<div xmlns=\'http://www.w3.org/1999/xhtml\'><p><b>Service Request</b></p>'\
+              '<p><b>Patient</b>: FRIAS,</p><p><b>Code</b>: Consult to Case Management</p>'\
+              '<p><b>Occurrence</b>: Dec  8, 2016  3:41 P.M. UTC</p><p><b>Priority</b>: Routine</p>'\
+              '<p><b>Status</b>: Revoked</p><p><b>Intent</b>: Order</p>'\
+              '<p><b>Requester</b>: Martin, MD, Mary Cerner</p></div>'
+            },
+            'status': 'revoked',
+            'intent': 'order',
+            'category': [
+              {
                 'coding': [
                   {
-                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/200',
-                    'code': '2551292823',
+                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/6000',
+                    'code': '636063',
+                    'display': 'Consults',
                     'userSelected': true
                   }
                 ],
-                'text': 'Consult to Case Management'
-              },
-              'subject': {
-                'reference': 'Patient/12457977',
-                'display': 'FRIAS,'
-              },
-              'encounter': {
-                'reference': 'Encounter/97697398'
-              },
-              'occurrenceDateTime': '2016-12-08T09:41:00.000-06:00',
-              'authoredOn': '2016-12-08T09:41:54.000-06:00',
-              'requester': {
-                'reference': 'Practitioner/11724002',
-                'display': 'Martin, MD, Mary Cerner'
+                'text': 'Consults'
               }
+            ],
+            'priority': 'routine',
+            'code': {
+              'coding': [
+                {
+                  'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/200',
+                  'code': '2551292823',
+                  'userSelected': true
+                }
+              ],
+              'text': 'Consult to Case Management'
+            },
+            'subject': {
+              'reference': 'Patient/12457977',
+              'display': 'FRIAS,'
+            },
+            'encounter': {
+              'reference': 'Encounter/97697398'
+            },
+            'occurrenceDateTime': '2016-12-08T09:41:00.000-06:00',
+            'authoredOn': '2016-12-08T09:41:54.000-06:00',
+            'requester': {
+              'reference': 'Practitioner/11724002',
+              'display': 'Martin, MD, Mary Cerner'
+            }
           }
         }
       ]
