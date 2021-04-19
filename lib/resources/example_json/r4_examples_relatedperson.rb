0c5e22ed629a0ae657c3165a87b595207ac2546d
AnanyaRamesh14 <hr.ananya14@gmail.com>
2021-04-19 18:50:56 +0530

    Updated changes for related person relationship read enhancement:indentation

--- lib/resources/example_json/r4_examples_relatedperson.rb
+++ lib/resources/example_json/r4_examples_relatedperson.rb
@@ -605,7 +605,7 @@ module Cerner
                 ],
                 'text': 'Sister'
               },
-                'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/relation'
+              'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/relation'
             },
             {
               'valuePeriod': {
@@ -1045,7 +1045,7 @@ module Cerner
                           'code': 'SIS',
                           'display': 'sister',
                           'userSelected': false
-                      }
+                        }
                       ],
                       'text': 'Sister'
                     },
