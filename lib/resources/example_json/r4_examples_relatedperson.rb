906c8894c70e01bd63fcd1eae7377f2bd18849c5
AnanyaRamesh14 <hr.ananya14@gmail.com>
2021-05-06 19:22:08 +0530

    Updated changes for related person relationship read enhancement:review comments

--- lib/resources/example_json/r4_examples_relatedperson.rb
+++ lib/resources/example_json/r4_examples_relatedperson.rb
@@ -245,6 +245,22 @@ module Cerner
             }
           ],
           'text': 'Default Guarantor'
+        },
+        {
+          'id': 'CI-97982588-1-0',
+          'coding': [
+            {
+              'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/40',
+              'code': '670847',
+              'display': 'Child',
+              'userSelected': true
+            },
+            {
+              'system': 'http://terminology.hl7.org/CodeSystem/v3-RoleCode',
+              'code': 'CHILD',
+              'display': 'child',
+              'userSelected': false
+            }
         }
       ],
       'name': [
@@ -519,16 +535,6 @@ module Cerner
               'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/351',
               'code': '1152',
               'display': 'Emergency Contact'
-            }
-          ]
-        },
-        {
-          'extension': [
-            {
-              'valuePeriod': {
-                'start': '2019-12-26T16:06:26.000Z'
-              },
-              'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/period'
             }
           ]
         }
