6ec0fd3ddd4f92916e1794dab260240eafd5f87d
Deepak Gupta <deepakgupta93.gkp@gmail.com>
2021-09-30 14:29:03 +0530

    Soarian R4 Practitioner Resource, example crrection

--- lib/resources/example_json/soarian_r4_examples_practitioner.rb
+++ lib/resources/example_json/soarian_r4_examples_practitioner.rb
@@ -5,39 +5,43 @@ module Cerner
 
     SOARIAN_R4_PRACTITIONER_SEARCH_BY_ID ||= {
       "resourceType": 'Bundle',
-      "id": '09f56536-7bae-4886-9dfc-3a5d77f2be28',
+      "id": 'a91a2e79-2379-4fdf-b9bd-4aee22fd5162',
       "type": 'searchset',
-      "timestamp": '2021-03-05T15:36:06-05:00',
+      "timestamp": '2021-09-30T04:00:27-04:00',
       "entry": [
         {
-          "fullUrl": 'https://fhir-myrecord-sc.sandboxcerner.com/r4/0e885770-571b-4c0c-b30f-21df9a058d0d/Practitioner/64FB91107E72432583430C3717ECF486',
+          "fullUrl": 'https://fhir-myrecord-sc.sandboxcerner.com/r4/3f2aca24-87f3-4eac-a6d7-1f75247e6b43/Practitioner/DF2821C049CC47E6A37C6B796DF4CF1D',
           "resource": {
             "resourceType": 'Practitioner',
-            "id": '64FB91107E72432583430C3717ECF486',
+            "id": 'DF2821C049CC47E6A37C6B796DF4CF1D',
             "meta": {
-              "lastUpdated": '2020-10-18T15:12:02-04:00'
+              "lastUpdated": '2021-01-07T13:56:11-05:00'
             },
             "name": [
               {
                 "use": 'official',
-                "text": 'Wullkotte,Joyce Anne-Marie',
-                "family": 'Wullkotte',
+                "text": 'Godziszewski,Janet Kris',
+                "family": 'Godziszewski',
                 "given": [
-                  'Joyce',
-                  'Anne-Marie'
+                  'Janet',
+                  'Kris'
+                ],
+                "suffix": [
+                  'MD'
                 ]
               }
             ],
+            "active": true,
             "identifier": [
               {
-                "system": 'urn:oid:12.00.021.1.3.2.10',
-                "value": 'LTC.184049'
+                "system": 'http://hl7.org/fhir/sid/us-npi',
+                "value": '7051690262'
               }
             ],
             "text": {
               "status": 'generated',
               # rubocop:disable Layout/LineLength
-              "div": "<div xmlns='http://www.w3.org/1999/xhtml'><b>Practitioner</b><br /><b>Practitioner</b>:Wullkotte,Joyce Anne-Marie<br /></div>"
+              "div": '<div xmlns="http://www.w3.org/1999/xhtml"><b>Practitioner</b><br /><b>Practitioner</b>:Godziszewski,Janet Kris<br /><b>Status</b>:Active<br /><b>Identifier:</b><b>System</b>:http://hl7.org/fhir/sid/us-npi,<b>Value</b>:7051690262<br /></div>'
               # rubocop:enable Layout/LineLength
             }
           }
@@ -46,38 +50,42 @@ module Cerner
       "link": [
         {
           "relation": 'self',
-          "url": 'https://fhir-myrecord-sc.sandboxcerner.com/r4/0e885770-571b-4c0c-b30f-21df9a058d0d/Practitioner?_id=64FB91107E72432583430C3717ECF486&_format=json'
+          "url": 'https://fhir-myrecord-sc.sandboxcerner.com/r4/3f2aca24-87f3-4eac-a6d7-1f75247e6b43/Practitioner?_id=DF2821C049CC47E6A37C6B796DF4CF1D&_format=json'
         }
       ]
     }.freeze
 
     SOARIAN_R4_PRACTITIONER_READ_BY_ID ||= {
       "resourceType": 'Practitioner',
-      "id": '64FB91107E72432583430C3717ECF486',
+      "id": 'DF2821C049CC47E6A37C6B796DF4CF1D',
       "meta": {
-        "lastUpdated": '2020-10-18T15:12:02-04:00'
+        "lastUpdated": '2021-01-07T13:56:11-05:00'
       },
       "name": [
         {
           "use": 'official',
-          "text": 'Wullkotte,Joyce Anne-Marie',
-          "family": 'Wullkotte',
+          "text": 'Godziszewski,Janet Kris',
+          "family": 'Godziszewski',
           "given": [
-            'Joyce',
-            'Anne-Marie'
+            'Janet',
+            'Kris'
+          ],
+          "suffix": [
+            'MD'
           ]
         }
       ],
+      "active": true,
       "identifier": [
         {
-          "system": 'urn:oid:12.00.021.1.3.2.10',
-          "value": 'LTC.184049'
+          "system": 'http://hl7.org/fhir/sid/us-npi',
+          "value": '7051690262'
         }
       ],
       "text": {
         "status": 'generated',
         # rubocop:disable Layout/LineLength
-        "div": "<div xmlns='http://www.w3.org/1999/xhtml'><b>Practitioner</b><br /><b>Practitioner</b>:Wullkotte,Joyce Anne-Marie<br /></div>"
+        "div": '<div xmlns="http://www.w3.org/1999/xhtml"><b>Practitioner</b><br /><b>Practitioner</b>:Godziszewski,Janet Kris<br /><b>Status</b>:Active<br /><b>Identifier:</b><b>System</b>:http://hl7.org/fhir/sid/us-npi,<b>Value</b>:7051690262<br /></div>'
         # rubocop:enable Layout/LineLength
       }
     }.freeze
