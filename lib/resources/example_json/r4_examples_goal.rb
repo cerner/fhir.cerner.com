4c5405a261bfbb8151492059a5c811c25b2b8045
khansa sultana <khansa.sultana@cerner.com>
2021-11-25 16:27:11 +0530

    R4 Goal: support _revinclude provennace for goal

--- lib/resources/example_json/r4_examples_goal.rb
+++ lib/resources/example_json/r4_examples_goal.rb
@@ -76,6 +76,173 @@ module Cerner
       ]
     }.freeze
 
+    R4_GOAL_REVINCLUDE_BUNDLE ||= {
+      "resourceType": 'Bundle',
+      "id": '4a1ae23d-cb80-49ac-9b3b-6abbdb7d5b92',
+      "type": 'searchset',
+      "link": [
+        {
+          "relation": 'self',
+          "url": 'https://fhir-ehr.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Goal?_id=498290085&_revinclude=Provenance%3Atarget'
+        }
+      ],
+      "entry": [
+        {
+          "fullUrl": 'https://fhir-ehr.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Goal/498290085',
+          "resource": {
+            "resourceType": 'Goal',
+            "id": '498290085',
+            "meta": {
+              "versionId": '1610019857000',
+              "lastUpdated": '2021-01-07T11:44:17.000Z'
+            },
+            "text": {
+              "status": 'generated',
+              "div": '<div xmlns="http://www.w3.org/1999/xhtml"><p><b>Goal</b></p><p>
+              <b>Subject</b>: Smith, Grace</p><p><b>Description</b>: Fighting depression</p>
+              <p><b>Expressed By</b>: Smith, Grace</p><p><b>Status</b>: Cancelled</p>
+              <p><b>Start Date</b>: Jan  7, 2021</p></div>'
+            },
+            "lifecycleStatus": 'cancelled',
+            "category": [
+              {
+                "coding": [
+                  {
+                    "system": 'https://fhir.cerner.com/2c400054-42d8-4e74-87b7-80b5bd5fde9f/codeSet/4003016',
+                    "code": '71690181',
+                    "display": 'Behavioral',
+                    "userSelected": true
+                  },
+                  {
+                    "system": 'http://terminology.hl7.org/CodeSystem/goal-category',
+                    "code": 'behavioral',
+                    "display": 'Behavioral',
+                    "userSelected": false
+                  }
+                ],
+                "text": 'Behavioral'
+              },
+              {
+                "coding": [
+                  {
+                    "system": 'https://fhir.cerner.com/2c400054-42d8-4e74-87b7-80b5bd5fde9f/codeSet/4003016',
+                    "code": '71690193',
+                    "display": 'Physiologic',
+                    "userSelected": true
+                  },
+                  {
+                    "system": 'http://terminology.hl7.org/CodeSystem/goal-category',
+                    "code": 'physiotherapy',
+                    "display": 'Physiotherapy',
+                    "userSelected": false
+                  }
+                ],
+                "text": 'Physiologic'
+              }
+            ],
+            "description": {
+              "text": 'Fighting depression'
+            },
+            "subject": {
+              "reference": 'Patient/2100031',
+              "display": 'Smith, Grace'
+            },
+            "startDate": '2021-01-07',
+            "statusDate": '2021-01-07',
+            "expressedBy": {
+              "reference": 'Patient/2100031',
+              "display": 'Smith, Grace'
+            }
+          },
+          "search": {
+            "mode": 'match'
+          }
+        },
+        {
+          "fullUrl": 'https://fhir-ehr.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Provenance/INT.goal-498290085',
+          "resource": {
+            "resourceType": 'Provenance',
+            "id": 'INT.goal-498290085',
+            "meta": {
+              "versionId": '498290085',
+              "lastUpdated": '2021-01-07T11:44:17Z'
+            },
+            "text": {
+              "status": 'generated',
+              "div": '<div xmlns="http://www.w3.org/1999/xhtml"><p><b>Provenance</b></p>
+              <p><b>Target</b>:</p><ul><li>Goal/498290085</li></ul><p><b>Recorded</b>: Jan  7, 2021  5:44 A.M. CST</p>
+              <p><b>Agents</b>:</p><dl><dt>One Plan, One Plan</dt><dd><b>Agent Type</b>: Author</dd>
+              <dd><b>Agent Role</b>: Source</dd><dt>DEVENG-DEFAULT-PROV</dt><dd><b>Agent Type</b>: Author</dd>
+              <dd><b>Agent Role</b>: Source</dd></dl></div>'
+            },
+            "target": [
+              {
+                "reference": 'Goal/498290085'
+              }
+            ],
+            "recorded": '2021-01-07T11:44:17Z',
+            "agent": [
+              {
+                "type": {
+                  "coding": [
+                    {
+                      "system": 'http://terminology.hl7.org/CodeSystem/provenance-participant-type',
+                      "code": 'author'
+                    }
+                  ],
+                  "text": 'Author'
+                },
+                "role": [
+                  {
+                    "coding": [
+                      {
+                        "system": 'http://terminology.hl7.org/CodeSystem/contractsignertypecodes',
+                        "code": 'SOURCE'
+                      }
+                    ],
+                    "text": 'Source'
+                  }
+                ],
+                "who": {
+                  "reference": 'Practitioner/118617332',
+                  "display": 'One Plan, One Plan'
+                }
+              },
+              {
+                "type": {
+                  "coding": [
+                    {
+                      "system": 'http://terminology.hl7.org/CodeSystem/provenance-participant-type',
+                      "code": 'author'
+                    }
+                  ],
+                  "text": 'Author'
+                },
+                "role": [
+                  {
+                    "coding": [
+                      {
+                        "system": 'http://terminology.hl7.org/CodeSystem/contractsignertypecodes',
+                        "code": 'SOURCE'
+                      }
+                    ],
+                    "text": 'Source'
+                  }
+                ],
+                "who": {
+                  "reference": 'Organization/57535538',
+                  "display": 'DEVENG-DEFAULT-PROV'
+                }
+              }
+            ]
+          },
+          "search": {
+            "mode": 'include'
+          }
+        }
+      ]
+    }.freeze
+
     R4_GOAL_BUNDLE ||= {
       'resourceType': 'Bundle',
       'id': 'c0dab9bc-3a6e-4f0f-b0b7-213ed80c3d4b',
