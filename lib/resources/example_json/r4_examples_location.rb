65dcbf5e783033bc077dbd863e1fc84deaaf8f10
OH092009 <Oleh_Hudak@epam.onmicrosoft.com>
2022-02-15 09:58:44 +0200

    OPENSVC-21503: - add identifier to all response examples

--- lib/resources/example_json/r4_examples_location.rb
+++ lib/resources/example_json/r4_examples_location.rb
@@ -124,6 +124,21 @@ module Cerner
         'Anushka Org One</p><p><b>Alias</b>: ADOne</p><p><b>Managing Organization</b>: Anushka Org One</p>'\
         '<p><b>Physical Type</b>: Facility(s)</p><p><b>Status</b>: Active</p></div>'
       },
+      'identifier': [
+        {
+          'use': 'official',
+          'type': {
+            'coding': [
+              'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/73',
+              'code': 'ACCTRECEIVABLE',
+              'display': 'FIN NBR',
+              'userSelected': true
+            ]
+          },
+          'system': 'FIN NBR',
+          'value': '16000',
+        }
+      ],
       "status": 'active',
       "name": 'Anushka Org One',
       "alias": [
@@ -226,6 +241,21 @@ module Cerner
               '<p><b>Managing Organization</b>: ABC Taxi Company</p> '\
               '<p><b>Physical Type</b>: Facility(s)</p><p><b>Status</b>: Active</p></div>'
             },
+            'identifier': [
+              {
+                'use': 'official',
+                'type': {
+                  'coding': [
+                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/73',
+                    'code': 'ACCTRECEIVABLE',
+                    'display': 'FIN NBR',
+                    'userSelected': true
+                  ]
+                },
+                'system': 'FIN NBR',
+                'value': '16000',
+              }
+            ],
             "status": 'active',
             "name": 'ABC Taxi Company',
             "alias": [
@@ -301,6 +331,21 @@ module Cerner
               '<p><b>Managing Organization</b>: ABC Taxi Company</p>'\
               '<p><b>Physical Type</b>: Facility(s)</p><p><b>Status</b>: Active</p></div>'
             },
+            'identifier': [
+              {
+                'use': 'official',
+                'type': {
+                  'coding': [
+                    'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/73',
+                    'code': 'ACCTRECEIVABLE',
+                    'display': 'FIN NBR',
+                    'userSelected': true
+                  ]
+                },
+                'system': 'FIN NBR',
+                'value': '16000',
+              }
+            ],
             "status": 'active',
             "name": 'ABC Taxi Company',
             "alias": [
