b0e02ba863f27f860cfb96bc9dae74d05cfa5bf0
Joseph Antony <51490169+joe-explr@users.noreply.github.com>
2023-08-11 20:27:20 +0530

    rubocop fixes

--- lib/resources/example_json/r4_examples_appointment.rb
+++ lib/resources/example_json/r4_examples_appointment.rb
@@ -523,21 +523,21 @@ module Cerner
       }
     ].freeze
 
-    R4_APPOINTMENT_EXTENSION_ACTION_COMMENT_PATCH ||=  [
+    R4_APPOINTMENT_EXTENSION_ACTION_COMMENT_PATCH ||= [
       {
-        "op": "replace",
-        "path": "/extension",
-        "value": [
+        'op': 'replace',
+        'path': '/extension',
+        'value': [
           {
-            "url": "https://fhir-ehr.cerner.com/r4/StructureDefinition/action-comment",
-            "valueString": "not required cancelled"
+            'url': 'https://fhir-ehr.cerner.com/r4/StructureDefinition/action-comment',
+            'valueString': 'not required cancelled'
           }
         ]
       },
       {
-        "op": "replace",
-        "path": "/status",
-        "value": "cancelled"
+        'op': 'replace',
+        'path': '/status',
+        'value': 'cancelled'
       }
     ].freeze
 
