112718553435b3554b2a2877872f69664a00ce19
Kennan Keim <kckeim@gmail.com>
2023-10-03 09:18:22 -0500

    Updated Ruby file with examples to please Rubocop

--- lib/resources/example_json/r4_examples_appointment.rb
+++ lib/resources/example_json/r4_examples_appointment.rb
@@ -3,38 +3,38 @@
 module Cerner
   module Resources
     R4_APPOINTMENT_ENTRY ||= {
-      "fullUrl": "https://fhir-ehr-code.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Appointment/4822366",
+      "fullUrl": 'https://fhir-ehr-code.cerner.com/r4/ec2458f2-1e24-41c8-b71b-0e701af7583d/Appointment/4822366',
       "resource": {
-        "resourceType": "Appointment",
-        "id": "4822366",
+        "resourceType": 'Appointment',
+        "id": '4822366',
         "meta": {
-          "versionId": "2",
-          "lastUpdated": "2021-04-22T12:21:26Z"
+          "versionId": '2',
+          "lastUpdated": '2021-04-22T12:21:26Z'
         },
         "text": {
-          "status": "generated",
-          "div": "<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Appointment</b></p><p><b>Status</b>: Cancelled</p><p><b>Service Type</b>: Surgery Rapid</p><p><b>Start</b>: Jan 23, 2020 10:10 P.M. UTC</p><p><b>End</b>: Jan 23, 2020 11:10 P.M. UTC</p><p><b>Participants</b>:</p><dl><dd><b>Location</b>: Endoscopy</dd><dd><b>Other Resource</b>: Endoscopy Rooms</dd><dd><b>Patient</b>: Smart II, Nancy</dd></dl></div>"
+          "status": 'generated',
+          "div": '<div xmlns=\"http://www.w3.org/1999/xhtml\"><p><b>Appointment</b></p><p><b>Status</b>: Cancelled</p><p><b>Service Type</b>: Surgery Rapid</p><p><b>Start</b>: Jan 23, 2020 10:10 P.M. UTC</p><p><b>End</b>: Jan 23, 2020 11:10 P.M. UTC</p><p><b>Participants</b>:</p><dl><dd><b>Location</b>: Endoscopy</dd><dd><b>Other Resource</b>: Endoscopy Rooms</dd><dd><b>Patient</b>: Smart II, Nancy</dd></dl></div>'
         },
         "extension": [
           {
-            "valueString": "",
-            "url": "https://fhir-ehr.cerner.com/r4/StructureDefinition/action-comment"
+            "valueString": '',
+            "url": 'https://fhir-ehr.cerner.com/r4/StructureDefinition/action-comment'
           },
           {
             "valueBoolean": false,
-            "url": "https://fhir-ehr.cerner.com/r4/StructureDefinition/is-cancelable"
+            "url": 'https://fhir-ehr.cerner.com/r4/StructureDefinition/is-cancelable'
           },
           {
             "valueBoolean": false,
-            "url": "https://fhir-ehr.cerner.com/r4/StructureDefinition/is-reschedulable"
+            "url": 'https://fhir-ehr.cerner.com/r4/StructureDefinition/is-reschedulable'
           }
         ],
-        "status": "cancelled",
+        "status": 'cancelled',
         "cancelationReason": {
           "coding": [
             {
-              "system": "https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14229",
-              "code": "0",
+              "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14229',
+              "code": '0',
               "userSelected": true
             }
           ]
@@ -43,94 +43,94 @@ module Cerner
           {
             "coding": [
               {
-                "system": "https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14249",
-                "code": "4047611",
-                "display": "Surgery Rapid",
+                "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14249',
+                "code": '4047611',
+                "display": 'Surgery Rapid',
                 "userSelected": true
               },
               {
-                "system": "http://snomed.info/sct",
-                "code": "394576009",
-                "display": "Accident & emergency (qualifier value)",
+                "system": 'http://snomed.info/sct',
+                "code": '394576009',
+                "display": 'Accident & emergency (qualifier value)',
                 "userSelected": false
               }
             ],
-            "text": "Surgery Rapid"
+            "text": 'Surgery Rapid'
           }
         ],
         "reasonCode": [
           {
-            "text": "I have a cramp"
+            "text": 'I have a cramp'
           }
         ],
-        "description": "Surgery Rapid",
-        "start": "2020-01-23T22:10:00Z",
-        "end": "2020-01-23T23:10:00Z",
+        "description": 'Surgery Rapid',
+        "start": '2020-01-23T22:10:00Z',
+        "end": '2020-01-23T23:10:00Z',
         "minutesDuration": 60,
         "participant": [
           {
             "actor": {
-              "reference": "Location/32216049",
-              "display": "Endoscopy"
+              "reference": 'Location/32216049',
+              "display": 'Endoscopy'
             },
-            "required": "required",
-            "status": "accepted"
+            "required": 'required',
+            "status": 'accepted'
           },
           {
             "type": [
               {
                 "coding": [
                   {
-                    "system": "https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14250",
-                    "code": "269829903",
-                    "display": "Endoscopy Rooms",
+                    "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14250',
+                    "code": '269829903',
+                    "display": 'Endoscopy Rooms',
                     "userSelected": true
                   }
                 ],
-                "text": "Endoscopy Rooms"
+                "text": 'Endoscopy Rooms'
               },
               {
                 "coding": [
                   {
-                    "system": "http://terminology.hl7.org/CodeSystem/v3-ParticipationType",
-                    "code": "PPRF",
-                    "display": "primary performer"
+                    "system": 'http://terminology.hl7.org/CodeSystem/v3-ParticipationType',
+                    "code": 'PPRF',
+                    "display": 'primary performer'
                   }
                 ]
               }
             ],
             "actor": {
-              "display": "Endoscopy Rooms"
+              "display": 'Endoscopy Rooms'
             },
-            "required": "required",
-            "status": "accepted"
+            "required": 'required',
+            "status": 'accepted'
           },
           {
             "type": [
               {
                 "coding": [
                   {
-                    "system": "https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14250",
-                    "code": "4572",
-                    "display": "Patient",
+                    "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/14250',
+                    "code": '4572',
+                    "display": 'Patient',
                     "userSelected": true
                   }
                 ],
-                "text": "Patient"
+                "text": 'Patient'
               }
             ],
             "actor": {
-              "reference": "Patient/12724066",
-              "display": "Smart II, Nancy"
+              "reference": 'Patient/12724066',
+              "display": 'Smart II, Nancy'
             },
-            "required": "required",
-            "status": "accepted"
+            "required": 'required',
+            "status": 'accepted'
           }
         ],
         "requestedPeriod": [
           {
-            "start": "2020-01-23T22:10:00Z",
-            "end": "2020-01-23T23:10:00Z"
+            "start": '2020-01-23T22:10:00Z',
+            "end": '2020-01-23T23:10:00Z'
           }
         ]
       }
