0ac5dd7ae2d42288b12a1c688dd158e96e22dad2
Aaron McGinn <aaron.mcginn@oracle.com>
2023-12-16 11:04:02 -0600

    Person updates for DSTU2 and R4 (#1009)

--- lib/resources/example_json/r4_examples_person.rb
+++ lib/resources/example_json/r4_examples_person.rb
@@ -37,160 +37,27 @@ module Cerner
       },
       'identifier': [
         {
-          'id': 'CI-490016886-0',
-          'use': 'usual',
-          'type': {
-            'coding': [
+          "id": 'CI-490525912-3',
+          "use": 'usual',
+          "type": {
+            "coding": [
               {
-                'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4',
-                'code': '10',
-                'display': 'MRN',
-                'userSelected': true
+                "system": 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4',
+                "code": '10',
+                "display": 'MRN',
+                "userSelected": true
               },
               {
-                'system': 'http://terminology.hl7.org/CodeSystem/v2-0203',
-                'code': 'MR',
-                'display': 'Medical record number',
-                'userSelected': false
+                "system": 'http://terminology.hl7.org/CodeSystem/v2-0203',
+                "code": 'MR',
+                "display": 'Medical record number',
+                "userSelected": false
               }
             ],
-            'text': 'MRN'
+            "text": 'MRN'
           },
-          'system': 'urn:oid:2.16.840.1.113883.6.1000',
-          'value': '6931',
-          'period': {
-            'start': '2019-12-26T15:14:12.000Z'
-          }
-        },
-        {
-          'id': 'CI-490058771-1',
-          'use': 'usual',
-          'type': {
-            'coding': [
-              {
-                'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4',
-                'code': '10',
-                'display': 'MRN',
-                'userSelected': true
-              },
-              {
-                'system': 'http://terminology.hl7.org/CodeSystem/v2-0203',
-                'code': 'MR',
-                'display': 'Medical record number',
-                'userSelected': false
-              }
-            ],
-            'text': 'MRN'
-          },
-          'system': 'urn:oid:2.16.840.1.113883.6.1000',
-          'value': '6978',
-          'period': {
-            'end': '2020-07-06T21:21:25.000Z'
-          }
-        },
-        {
-          'id': 'CI-490059574-3',
-          'use': 'usual',
-          'type': {
-            'coding': [
-              {
-                'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4',
-                'code': '670843',
-                'display': 'Messaging',
-                'userSelected': true
-              },
-              {
-                'system': 'http://terminology.hl7.org/CodeSystem/v2-0203',
-                'code': 'U',
-                'display': 'Unspecified identifier',
-                'userSelected': false
-              }
-            ],
-            'text': 'Messaging'
-          },
-          'value': '3C36293A3B964994AD8E6C0305F3330A',
-          'period': {
-            'start': '2020-06-30T20:08:26.000Z'
-          }
-        },
-        {
-          'id': 'CI-490058805-4',
-          'use': 'usual',
-          'type': {
-            'coding': [
-              {
-                'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4',
-                'code': '670843',
-                'display': 'Messaging',
-                'userSelected': true
-              },
-              {
-                'system': 'http://terminology.hl7.org/CodeSystem/v2-0203',
-                'code': 'U',
-                'display': 'Unspecified identifier',
-                'userSelected': false
-              }
-            ],
-            'text': 'Messaging'
-          },
-          'value': '43DA797A657B47548F258A9B50EB41F5',
-          'period': {
-            'start': '2020-06-12T16:03:32.000Z'
-          }
-        },
-        {
-          'id': 'CI-490059570-5',
-          'use': 'usual',
-          'type': {
-            'coding': [
-              {
-                'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4',
-                'code': '2553236771',
-                'display': 'Federated Person Principal',
-                'userSelected': true
-              },
-              {
-                'system': 'http://terminology.hl7.org/CodeSystem/v2-0203',
-                'code': 'AN',
-                'display': 'Account number',
-                'userSelected': false
-              }
-            ],
-            'text': 'Federated Person Principal'
-          },
-          'system': 'urn:oid:2.16.840.1.113883.3.13.6',
-          'value': 'URN:CERNER:IDENTITY-FEDERATION:REALM:E8A84236-C258-4952-98B7-A6FF8A9C587A-CH:PRINCIPAL:'\
-                   'AN7TD9A62CV8Z53Z',
-          'period': {
-            'start': '2020-06-30T20:08:25.000Z'
-          }
-        },
-        {
-          'id': 'CI-490058801-6',
-          'use': 'usual',
-          'type': {
-            'coding': [
-              {
-                'system': 'https://fhir.cerner.com/ec2458f2-1e24-41c8-b71b-0e701af7583d/codeSet/4',
-                'code': '2553236771',
-                'display': 'Federated Person Principal',
-                'userSelected': true
-              },
-              {
-                'system': 'http://terminology.hl7.org/CodeSystem/v2-0203',
-                'code': 'AN',
-                'display': 'Account number',
-                'userSelected': false
-              }
-            ],
-            'text': 'Federated Person Principal'
-          },
-          'system': 'urn:oid:2.16.840.1.113883.3.13.6',
-          'value': 'URN:CERNER:IDENTITY-FEDERATION:REALM:E8A84236-C258-4952-98B7-A6FF8A9C587A-CH:PRINCIPAL:'\
-                   'KR8KC9MI9EQ8KC23',
-          'period': {
-            'start': '2020-06-12T16:03:29.000Z'
-          }
+          "system": 'urn:oid:2.16.840.1.113883.6.1000',
+          "value": '31577'
         }
       ],
       'name': [
@@ -204,19 +71,6 @@ module Cerner
           ],
           'period': {
             'start': '2019-12-26T15:14:12.000Z'
-          }
-        },
-        {
-          'id': 'CI-490059796-0',
-          'use': 'old',
-          'text': 'SMART, STEPHEN ALLEN',
-          'family': 'SMART',
-          'given': [
-            'STEPHEN',
-            'ALLEN'
-          ],
-          'period': {
-            'end': '2020-07-06T21:21:26.000Z'
           }
         }
       ],
@@ -240,7 +94,7 @@ module Cerner
         {
           'id': 'CI-EM-29822662-0',
           'system': 'email',
-          'value': 'joesmart@yopmail.com',
+          'value': 'joesmart@example.com',
           'use': 'home',
           'rank': '1',
           'period': {
@@ -254,11 +108,11 @@ module Cerner
         {
           'id': 'CI-24313553-0',
           'use': 'home',
-          'text': '12345 Main St\nKansas city, MO 64116\nUS',
+          'text': '12345 Main St\nKansas City, MO 64116\nUS',
           'line': [
             '12345 Main St'
           ],
-          'city': 'Kansas city',
+          'city': 'Kansas City',
           'district': 'Jackson',
           'state': 'MO',
           'postalCode': '64116',
